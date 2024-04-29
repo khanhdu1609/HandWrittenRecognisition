51826cad8e61a1a2845048f1736b020506de7b3e
khanhdu1609 <141617409+khanhdu1609@users.noreply.github.com>
2024-04-29 20:56:52 +0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -3,16 +3,17 @@ from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precisio
 import torch
 from MnistModule import MnistModule
 if __name__ == '__main__':
+    mnistModule = MnistModule()
     model = torch.hub.load('/content/drive/MyDrive/digit_classifier_model.pt')
     #test path
     test_images_path = '/content/drive/MyDrive/t10k-images-idx3-ubyte.gz'
     test_labels_path = '/content/drive/MyDrive/t10k-labels-idx1-ubyte.gz'
-    test_images, test_labels = MnistModule.get_images_labels(test_images_path, test_labels_path, 10000)
+    test_images, test_labels = mnistModule.get_images_labels(test_images_path, test_labels_path, 10000)
 
-    predictions = MnistModule.predict(test_images)
+    predictions = mnistModule.predict(test_images)
 
-    MnistModule.get_accuracy(test_labels)
+    mnistModule.get_accuracy(test_labels)
 
-    MnistModule.get_f1_score(test_labels)
+    mnistModule.get_f1_score(test_labels)
 
-    MnistModule.get_precision_score(test_labels)
+    mnistModule.get_precision_score(test_labels)
