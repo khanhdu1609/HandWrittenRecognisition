c1ce3baa29c29bfbfa08de23177385d36b649065
khanhdu1609 <141617409+khanhdu1609@users.noreply.github.com>
2024-04-29 20:53:56 +0700

    Update train.py

--- train.py
+++ train.py
@@ -2,14 +2,15 @@ from model import ClassifierModel
 from MnistModule import MnistModule
 if __name__ == '__main__':
     # train path
+    mnistModule = MnistModule()
     train_images_path = '/content/drive/MyDrive/train-images-idx3-ubyte.gz'
     train_labels_path = '/content/drive/MyDrive/train-labels-idx1-ubyte.gz'
-    train_images, train_labels = MnistModule.get_images_labels(train_images_path, train_labels_path, 60000)
-    train_loader = MnistModule.load_data(train_images, train_labels)
+    train_images, train_labels = mnistModule.get_images_labels(train_images_path, train_labels_path, 60000)
+    train_loader = mnistModule.load_data(train_images, train_labels)
     #define model
     model = ClassifierModel()
     #train model
-    model = MnistModule.train(model, train_loader)
+    model = mnistModule.train(model, train_loader)
     #save model 
     path = '/content/drive/MyDrive/digit_classifier_model.pt'
     MnistModule.save(model, path)
